a5de4cf0c4397d027b2d12e26b446cd15a1a0241
AlexGit2012 <a.karavaychik@gmail.com>
2020-01-21 21:48:31 +0300

    delete withcredentials header

--- front/src/Loginization/reducer/api.ts
+++ front/src/Loginization/reducer/api.ts
@@ -3,8 +3,8 @@ import {APIerrorLogger} from "../../utils/errorLogger";
 import {I_loginData, I_registerData} from "../../types/types";
 
 const instance = axios.create({
-    baseURL: "https://dry-forest-56016.herokuapp.com/auth",
-    withCredentials: true
+    baseURL: "https://dry-forest-56016.herokuapp.com/auth"
+    /*withCredentials: true*/
 });
 
 export const authAPI = {
@@ -13,7 +13,7 @@ export const authAPI = {
             debugger
             let response = await instance.post(`/login`, data)
             debugger
-            return response
+            return response.data
         } catch (err) {
             debugger;
             APIerrorLogger(err);
